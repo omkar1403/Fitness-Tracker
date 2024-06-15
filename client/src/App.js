dbd1eb75d3ec69716da08bbed567ede0b0cb42a7
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-06-16 01:04:00 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import {BrowserRouter} from "react-router-dom"
+import Authentication from "./pages/Authentication";
+
+const Container=styled.div`
+width:100%;
+height:100vh;
+display:flex;
+flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      Home
-    </div>
-  );
+  return <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+      <Container>
+        <Authentication/>
+      </Container>
+      </BrowserRouter>
+    </ThemeProvider>;
+
 }
 
 export default App;
