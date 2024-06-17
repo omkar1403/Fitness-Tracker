f34085414b7356e8c8703fd06c11440137cd5172
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-06-18 01:39:44 +0530

    xyz

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: "https://fitness-tracker-ap.vercel.app/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
