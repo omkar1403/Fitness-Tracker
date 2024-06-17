ddd0c337f16b76acf0b9c1e936f6de6a6b111038
omkar1403 <155137999+omkar1403@users.noreply.github.com>
2024-06-18 02:07:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,6 +26,12 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get("/", async (req, res) => {
   res.status(200).json({
     message: "Hello developers from GFG",
@@ -33,11 +39,7 @@ app.get("/", async (req, res) => {
 });
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 
 app.use("/api/user/", UserRoutes);
 
