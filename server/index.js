abefe23ad2f33adc00cefe2e5e30a37b6563649d
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2024-06-18 01:54:37 +0530

    xyz

--- server/index.js
+++ server/index.js
@@ -9,17 +9,17 @@ dotenv.config();
 const app = express();
 
 // CORS configuration
-const allowedOrigins = ['http://localhost:3000']; // Add other allowed origins here
+const allowedOrigins = ['https://fitness-tracker-frontend-teal.vercel.app/']; // Add other allowed origins here
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT'], // Specify allowed methods
-    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT'], // Specify allowed methods
+  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
 };
 
 app.use(cors(corsOptions));
@@ -27,41 +27,41 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
 app.get("/", async (req, res) => {
-    res.status(200).json({
-        message: "Hello developers from GFG",
-    });
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
 });
 
 app.use("/api/user/", UserRoutes);
 
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong";
-    return res.status(status).json({
-        success: false,
-        status,
-        message,
-    });
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
 });
 
 const connectDB = () => {
-    mongoose.set("strictQuery", true);
-    mongoose
-        .connect(process.env.MONGODB_URL)
-        .then(() => console.log("Connected to Mongo DB"))
-        .catch((err) => {
-            console.error("Failed to connect with MongoDB");
-            console.error(err);
-        });
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("Failed to connect with MongoDB");
+      console.error(err);
+    });
 };
 
 const startServer = async () => {
-    try {
-        connectDB();
-        app.listen(8080, () => console.log("Server started on port 8080"));
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
